d2e3c7122cd0873b095fc695b5228caa57fccfd7
Gelvyn-silvzz <gelvyn.silva@escola.pr.gov.br>
2025-08-15 09:21:18 -0300

    Add files via upload

---
+++ main (1).sql
@@ -0,0 +1,20 @@
+CREATE TABLE continente (
+    id_continente INTEGER PRIMARY KEY NOT NULL ,
+    nome_continente VARCHAR(40) NOT NULL 
+);
+INSERT INTO continente VALUES (
+        1,
+        'ASIA'
+);
+INSERT INTO continente VALUES (
+        2,
+        'OCEANIA'
+);
+INSERT INTO continente VALUES (
+        3,
+        'EUROPA'
+);
+INSERT INTO continente VALUES (
+        4,
+        'AMERICA'
+);
